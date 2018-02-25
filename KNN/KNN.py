b9583ef341450e4bd3711b54a06e6e87001ad378
zhanghuajieHIT <310336422@qq.com>
2018-02-25 15:00:22 +0800

    采用sublime text编写脚本

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python2
-# -*- coding:utf8 -*-
-# @TIME   : 2/22/18 11:28 PM
-# @Author : ZHJ
-# @File   : KNN.py
+# -*- coding: utf-8 -*-
+# @Author: zhanghuajieHIT
+# @Date:   2018-02-25 14:30:47
+# @Last Modified by:   zhanghuajieHIT
+# @Last Modified time: 2018-02-25 14:58:29
 
 from numpy import *
 import operator
@@ -15,5 +15,20 @@ def createDataSet():
 
 
 def classify(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
+	dataSetSize = dataSet.shape[0]
+	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+	sqDiffMat = diffMat ** 2
+	sqDistance =sqDiffMat.sum(axis = 1)
+	distances = sqDistance ** 0.5
+	sortedDistIndicies = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndicies[i]]
+		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+	sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
+	return sortedClassCount[0][0]
 
+if __name__ == "__main__":
+	group, labels = createDataSet()
+	result = classify([0, 0], group, labels, 3)
+	print (result)
